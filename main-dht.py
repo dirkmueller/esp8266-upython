0b2af408b12c11e5a3eb448be29f613d6cb78e0d
Dirk Mueller <dirk@dmllr.de>
2018-11-25 16:08:20 +0100

    Report absolute humidity as well

--- main-dht.py
+++ main-dht.py
@@ -1,4 +1,5 @@
 import gc
+import math
 import sys
 
 if sys.implementation.name == 'micropython':
@@ -7,6 +8,7 @@ if sys.implementation.name == 'micropython':
     import esp
     import network
     import usocket
+    import utime
 
     wlan = network.WLAN(network.STA_IF)
     dht_sensor = dht.DHT22(machine.Pin(13))
@@ -22,7 +24,7 @@ if sys.implementation.name == 'micropython':
 else:
     import socket as usocket
     import random
-    import time
+    import time as utime
     wlan = None
     rtc = None
     machineid = 'host1'
@@ -33,7 +35,7 @@ def get_measure():
         dht_sensor.measure()
         return dht_sensor.temperature(), dht_sensor.humidity()
 
-    return random.randint(5, 10), random.randint(40, 60)
+    return random.randint(180, 220)/10, 42
 
 
 desthost = 'air.dmllr.de'
@@ -54,7 +56,7 @@ def do_connect():
         print(' connected as:', wlan.ifconfig()[0])
 
 
-def send_measurement(temperature, humidity):
+def send_measurement(temperature, humidity, abs_humidity):
     if sys.implementation.name != 'micropython':
         print('Not sending')
         return
@@ -64,8 +66,8 @@ def send_measurement(temperature, humidity):
         s.connect(usocket.getaddrinfo(desthost, 80)[0][-1])
 
         data_to_send = (
-            'templog,host=%s temperature=%f,humidity=%f') % (
-             machineid, temperature, humidity)
+            'templog,host=%s temperature=%f,humidity=%f,abshumidity=%f') % (
+             machineid, temperature, humidity, abs_humidity)
 
         req = '''POST /TVgsSPvTSLMC6/write?db=templogger HTTP/1.1
 Host: %(hostname)s
@@ -74,8 +76,7 @@ Content-Length: %(postlength)d
 Content-Type: application/x-www-form-urlencoded
 
 %(body)s
-''' % ({'hostname': desthost,
-        'postlength': len(data_to_send),
+''' % ({'hostname': desthost, 'postlength': len(data_to_send),
         'body': data_to_send})
 
         s.sendall(req.encode())
@@ -87,30 +88,65 @@ Content-Type: application/x-www-form-urlencoded
         s.close()
 
 
-def do_sleep():
-    sleeptime = 3 * 60 + 20
+def get_ah(temperature, humidity):
+    # from https://carnotcycle.wordpress.com/2012/08/04/
+    #      how-to-convert-relative-humidity-to-absolute-humidity/
 
-    if rtc is None:
-        time.sleep(sleeptime)
-        return
+    return ((13.2471 * humidity *
+             math.exp((17.67 * temperature) / (temperature + 243.5))) /
+            (273.15 + temperature))
 
-    rtc.alarm(0, sleeptime*1000)
-    while(rtc.alarm_left()):
-        machine.idle()
+
+def do_sleep(sleeptime):
+
+    utime.sleep(sleeptime)
     print('> Wakup from sleep')
     gc.collect()
 
 
+do_loop_samples = []
+
+
 def do_loop():
-    temp, humidity = get_measure()
-    print('Current temp %f (%f %% Humidity)' % (temp, humidity))
+    global do_loop_samples
+    samples = do_loop_samples
+    condition = True
+    while condition:
+        temp, humidity = get_measure()
+        abs_humidity = get_ah(temp, humidity)
+
+        sample = (temp, humidity, abs_humidity)
+
+        print('[%02d:%02d:%02d.%03d]' % utime.localtime()[3:7],
+              ' %.01f C (%.01f %% rel.H %4.1f abs.H)' % sample)
+
+        samples.append((utime.time(), sample))
+
+        if (hasattr(utime, 'sleep_ms')):
+            utime.sleep_ms(1000 - utime.localtime()[7] + 30 * 1000)
+
+        print('# Samples: ', len(samples))
+
+        condition = (utime.time() - samples[0][0] < 10 * 60 and
+                     # less than 0.1 C difference
+                     abs(samples[0][1][0] - sample[0]) < 0.1 and
+                     # less than 0.3 % humidity difference
+                     abs(samples[0][1][1] - sample[1]) < 0.3)
 
     do_connect()
-    send_measurement(temp, humidity)
+    current_sample = samples.pop()
+    send_measurement(*(current_sample[1]))
+    do_loop_samples = [current_sample]
 
 
 while True:
     do_loop()
 
-    do_sleep()
+    hour_of_day = utime.localtime()[3]
+
+    if hour_of_day in range(0, 4):
+        do_sleep(7 * 60 + 20)
+    else:
+        do_sleep(2 * 60 + 10)
+
     print('.')
